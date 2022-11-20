fd4b4583a1cb046c8b71f6466767e25f8a31cfed
Amstramgram75 <laurentneumann@free.fr>
2022-11-20 15:51:44 +0100

    Initial

---
+++ docs/js/polyfills/polyfill.js
@@ -0,0 +1,1 @@
+!function(){"use strict";!function(){if("function"!=typeof window.CustomEvent){function t(t,n){n=n||{bubbles:!1,cancelable:!1,detail:null};var e=document.createEvent("CustomEvent");return e.initCustomEvent(t,n.bubbles,n.cancelable,n.detail),e}window.CustomEvent=t}}()}();
